c2cd68a929cdb0a04f3d3cd36a72f15f82f968c0
Adam Bachrach <nalc1100adam@gmail.com>
2022-06-16 16:35:50 -0400

    fetch more data from the API

--- browse-movies.js
+++ browse-movies.js
@@ -1,30 +1,31 @@
 const apiUrl =
-  "https://api.themoviedb.org/3/genre/movie/list?api_key=9c05bfa9fd21923d33ba2cdcfc64c52c&language=en-US";
-let searchString = "";
+  "https://api.themoviedb.org/3/genre/movie/list?api_key=9c05bfa9fd21923d33ba2cdcfc64c52c&language=en-US"
+let searchString = ""
 
 function fetchIt() {
   fetch(`${apiUrl}${searchString}`)
     .then((result) => result.json())
     .then((result) => {
       result.genres.forEach((element) => {
-        genreList.push(element.name);
-      });
-      genreDropdownGen();
+        genreList.push(element)
+        console.log(element.id)
+      })
+      genreDropdownGen()
     })
     .catch((err) => {
-      console.error("Error --- ", err);
-    });
+      console.error("Error --- ", err)
+    })
 }
-fetchIt();
+fetchIt()
 
-const genreList = [];
-const genreDropdown = document.querySelector("#genre-select");
+const genreList = []
+const genreDropdown = document.querySelector("#genre-select")
 
 function genreDropdownGen() {
   genreList.forEach((genre) => {
-    let option = document.createElement("option");
-    option.value = genre;
-    option.innerText = genre;
-    genreDropdown.appendChild(option);
-  });
+    let option = document.createElement("option")
+    option.value = genre.name
+    option.innerText = genre.name
+    genreDropdown.appendChild(option)
+  })
 }
