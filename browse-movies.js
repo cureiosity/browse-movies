ab3cc4ce6868ce1d82fba289f161ace8394bd835
Adam Bachrach <nalc1100adam@gmail.com>
2022-06-16 15:28:42 -0400

    populate a genre <select> element with genre data from the API

--- browse-movies.js
+++ browse-movies.js
@@ -1,14 +1,30 @@
-const apiUrl = 'https://api.themoviedb.org/3/search/movie?api_key=9c05bfa9fd21923d33ba2cdcfc64c52c&query='
-let searchString = 'bacurau'
+const apiUrl =
+  "https://api.themoviedb.org/3/genre/movie/list?api_key=9c05bfa9fd21923d33ba2cdcfc64c52c&language=en-US";
+let searchString = "";
 
 function fetchIt() {
   fetch(`${apiUrl}${searchString}`)
-    .then(result => result.json())
-    .then(result => {
-      console.log(result)
+    .then((result) => result.json())
+    .then((result) => {
+      result.genres.forEach((element) => {
+        genreList.push(element.name);
+      });
+      genreDropdownGen();
     })
-    .catch(err => {
-      console.error("Error --- ", err)
-    })
+    .catch((err) => {
+      console.error("Error --- ", err);
+    });
 }
-fetchIt()
+fetchIt();
+
+const genreList = [];
+const genreDropdown = document.querySelector("#genre-select");
+
+function genreDropdownGen() {
+  genreList.forEach((genre) => {
+    let option = document.createElement("option");
+    option.value = genre;
+    option.innerText = genre;
+    genreDropdown.appendChild(option);
+  });
+}
